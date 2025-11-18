31f9fd124f5635afd45593e929d0a98230f34591
Joan Puigcerver Ibáñez <j.puigcerveribanez@edu.gva.es>
2025-11-18 12:46:04 +0100

    p2/invalid: null

--- src/test/java/ud4/methods/PasswordStrengthCheckerTest.java
+++ src/test/java/ud4/methods/PasswordStrengthCheckerTest.java
@@ -1,7 +1,28 @@
 package ud4.methods;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+import ud4.methods.PasswordStrengthChecker.PasswordStrength;
 
 class PasswordStrengthCheckerTest {
+    @Nested
+    @DisplayName("Invalid tests")
+    class InvalidTests {
+        @Test
+        @DisplayName("Null password")
+        void nullPasswordShouldBeInvalid(){
+            // ARRANGE
+            String password = null;
+            PasswordStrength expected = PasswordStrength.INVALID;
 
+            // ACT
+            PasswordStrength actual = PasswordStrengthChecker.checkStrength(password);
+
+            // ASSERT
+            assertEquals(expected, actual);
+        }
+    }
 }
