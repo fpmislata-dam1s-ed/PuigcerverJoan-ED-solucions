307aefdf77bbb08630b0af1c361acc750f0f97ec
Joan Puigcerver Ibáñez <j.puigcerveribanez@edu.gva.es>
2025-11-18 13:00:16 +0100

    p2/strong: lower & upper case & symbols

--- src/test/java/ud4/methods/PasswordStrengthCheckerTest.java
+++ src/test/java/ud4/methods/PasswordStrengthCheckerTest.java
@@ -2,6 +2,24 @@ package ud4.methods;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class PasswordStrengthCheckerTest {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import ud4.methods.PasswordStrengthChecker.PasswordStrength;
 
+class PasswordStrengthCheckerTest {
+    @Nested
+    @DisplayName("Strong testcases")
+    class StrongTests {
+        @Test
+        @DisplayName("lower & upper case & symbols")
+        void lowerAndUpperCaseAndSymbolsShouldBeStrong(){
+            String password = "aBc*/DeF";
+            PasswordStrength expected = PasswordStrength.STRONG;
+
+
+            PasswordStrength actual = PasswordStrengthChecker.checkStrength(password);
+            assertEquals(expected, actual);
+        }
+    }
 }
