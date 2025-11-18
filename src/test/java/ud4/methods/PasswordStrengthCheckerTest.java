c4b90b39ded81cbb9a87c8f871de6c3ce0562ac3
Joan Puigcerver Ibáñez <j.puigcerveribanez@edu.gva.es>
2025-11-18 12:50:18 +0100

    p2/weak: lower case

--- src/test/java/ud4/methods/PasswordStrengthCheckerTest.java
+++ src/test/java/ud4/methods/PasswordStrengthCheckerTest.java
@@ -1,7 +1,24 @@
 package ud4.methods;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+import ud4.methods.PasswordStrengthChecker.PasswordStrength;
 
 class PasswordStrengthCheckerTest {
+    @Nested
+    @DisplayName("Weak testcases")
+    class WeakTests {
+        @Test
+        @DisplayName("Lower case")
+        void lowerCasePasswordShouldBeWeak(){
+            String password = "abcdefgh";
+            PasswordStrength expected = PasswordStrength.WEAK;
 
+            PasswordStrength actual = PasswordStrengthChecker.checkStrength(password);
+            assertEquals(expected, actual);
+        }
+    }
 }
