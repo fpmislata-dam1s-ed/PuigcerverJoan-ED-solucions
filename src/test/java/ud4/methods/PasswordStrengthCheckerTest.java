3e2ca79abda9dba1aa44b0754842136771789e76
Joan Puigcerver Ibáñez <j.puigcerveribanez@edu.gva.es>
2025-11-18 12:55:48 +0100

    p2/medium: lower & upper case

--- src/test/java/ud4/methods/PasswordStrengthCheckerTest.java
+++ src/test/java/ud4/methods/PasswordStrengthCheckerTest.java
@@ -2,6 +2,22 @@ package ud4.methods;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import ud4.methods.PasswordStrengthChecker.PasswordStrength;
 class PasswordStrengthCheckerTest {
+    @Nested
+    @DisplayName("Medium testcases")
+    class MediumTests {
+        @Test
+        @DisplayName("lower & upper case")
+        void lowerAndUpperCaseShouldBeMedium(){
+            String password = "aBcDeFgH";
+            PasswordStrength expected = PasswordStrength.MEDIUM;
 
+            PasswordStrength actual = PasswordStrengthChecker.checkStrength(password);
+            assertEquals(expected, actual);
+        }
+    }
 }
