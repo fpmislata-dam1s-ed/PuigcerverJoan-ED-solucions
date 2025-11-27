e386f6a06f1ba3045bf9a3144a4430c7f44b68f6
Joan Puigcerver Ibáñez <j.puigcerveribanez@edu.gva.es>
2025-11-27 10:32:32 +0100

    ex1: tests with vowels

--- src/test/java/exam1/ContainsVowelTest.java
+++ src/test/java/exam1/ContainsVowelTest.java
@@ -1,7 +1,52 @@
 package exam1;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ContainsVowelTest {
+    @Nested
+    @DisplayName("Tests with vowels")
+    class VowelsTests {
+        @Test
+        @DisplayName("Text with lower case vowels")
+        void textLowerCaseVowelsShouldReturnTrue(){
+            // Arrange
+            String word = "hello";
 
+            // Act
+            boolean actual = ContainsVowel.containsVowel(word);
+
+            // Assert
+            assertTrue(actual);
+        }
+
+        @Test
+        @DisplayName("Text with upper case vowels")
+        void textUpperCaseVowelsShouldReturnTrue(){
+            // Arrange
+            String word = "HELLO";
+
+            // Act
+            boolean actual = ContainsVowel.containsVowel(word);
+
+            // Assert
+            assertTrue(actual);
+        }
+
+        @Test
+        @DisplayName("Text with upper & lower case vowels")
+        void textLowerAndUpperCaseVowelsShouldReturnTrue(){
+            // Arrange
+            String word = "HelLO";
+
+            // Act
+            boolean actual = ContainsVowel.containsVowel(word);
+
+            // Assert
+            assertTrue(actual);
+        }
+    }
 }
