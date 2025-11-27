821b16ff670962666641314d4e383f1805a14413
Joan Puigcerver Ibáñez <j.puigcerveribanez@edu.gva.es>
2025-11-27 10:39:59 +0100

    ex1: tests with no vowels

--- src/test/java/exam1/ContainsVowelTest.java
+++ src/test/java/exam1/ContainsVowelTest.java
@@ -1,7 +1,75 @@
 package exam1;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ContainsVowelTest {
+    @Nested
+    @DisplayName("Tests without vowels")
+    class VowelsTests {
+        @Test
+        @DisplayName("empty string")
+        void emptyStringShouldReturnFalse(){
+            // Arrange
+            String word = "";
 
+            // Act
+            boolean actual = ContainsVowel.containsVowel(word);
+
+            // Assert
+            assertFalse(actual);
+        }
+
+        @Test
+        @DisplayName("null string")
+        void nullStringShouldReturnFalse(){
+            // Act
+            boolean actual = ContainsVowel.containsVowel(null);
+
+            // Assert
+            assertFalse(actual);
+        }
+
+        @Test
+        @DisplayName("Text with lower case no vowels")
+        void textLowerCaseVowelsShouldReturnFalse(){
+            // Arrange
+            String word = "crypt";
+
+            // Act
+            boolean actual = ContainsVowel.containsVowel(word);
+
+            // Assert
+            assertFalse(actual);
+        }
+
+        @Test
+        @DisplayName("Text with upper case no vowels")
+        void textUpperCaseVowelsShouldReturnFalse(){
+            // Arrange
+            String word = "CRYPT";
+
+            // Act
+            boolean actual = ContainsVowel.containsVowel(word);
+
+            // Assert
+            assertFalse(actual);
+        }
+
+        @Test
+        @DisplayName("Text with upper & lower case no vowels")
+        void textLowerAndUpperCaseVowelsShouldReturnFalse(){
+            // Arrange
+            String word = "CryPt";
+
+            // Act
+            boolean actual = ContainsVowel.containsVowel(word);
+
+            // Assert
+            assertFalse(actual);
+        }
+    }
 }
